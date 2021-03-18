8cbd51817e77da6af1938ae872fee29f34cb3fb4
Samuel Sjöbergsson <33666789+sjobergsson@users.noreply.github.com>
2021-03-18 11:02:52 +0100

    Double reports
    Now this scripts export both a textfile with tags per site and how many aps any tag has and a csv with all aps with ap-name, sitename, tag and mac-adress.

--- AP-report.py
+++ AP-report.py
@@ -1,20 +1,19 @@
+# coding: utf8
 import requests
 import json
 import csv
 import time
 
 from datetime import datetime
+from re import search
 
 import logging
 logging.basicConfig(filename='logg.log', encoding='utf-8', level=logging.DEBUG)
-#Edit base-url if you are not using the EU-environment
+
 base_url = 'https://api.eu.mist.com/api/v1/'
-#The organisation-ID for your organisation
 org_id = '...'
-#Your MIST API token
+
 mist_token = '...'
-
-
 authorization = "Token {}".format(mist_token)
 
 headers = {
@@ -22,71 +21,96 @@ headers = {
     'Authorization': authorization
 }
 
-report_file = 'reports/AP_Inventory_Org_' + \
+#Regex for filtering out so that we only show sites that have a shortname
+# which is how we decide if a site is just for lab or a actual site
+regex_sitename = ".*\(....\)"
+#Regex for empty json to hide sites without any accesspoints.
+regex_empty = "^\[\]$"
+
+report_file_site = 'reports/Site_Inventory_Org_' + \
+    str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.txt'
+
+report_file_ap = 'reports/AP_Inventory_Org_' + \
     str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + '.csv'
+
+file_report = open(report_file_site, "a", encoding="utf8")
 
 sites_url = "{}/orgs/{}/sites".format(base_url, org_id)
 
 resultssites = requests.get(sites_url, headers=headers)
 sites = json.loads(resultssites.text)
 
+
 dict_data = []
 
 for site in sites:
-    print(site['name'])
-    #Log name and ID to file
-    logging.info(site['name'])
-    logging.info('Site-ID:')
-    logging.info(site['id'])
-    #Get devices on site
-    device_url = "{}/sites/{}/devices".format(base_url, site["id"])
-    resultsdevices = requests.get(device_url, headers=headers)
-    devices = json.loads(resultsdevices.text)
-    #Get tags on site
-    tags_url = "{}/sites/{}/wxtags".format(base_url, site["id"])
-    resultstags = requests.get(tags_url, headers=headers)
-    tags = json.loads(resultstags.text)
+    if search(regex_sitename, site['name']):
 
-    device_lookup = {}
+        #Log name and ID to file
+        logging.info(site['name'])
+        logging.info('Site-ID:')
+        logging.info(site['id'])
+        #Get devices on site
+        device_url = "{}/sites/{}/devices".format(base_url, site["id"])
+        resultsdevices = requests.get(device_url, headers=headers)
+        devices = json.loads(resultsdevices.text)
+        if not search(regex_empty, str(devices)):
+            print(site['name'])
+            site_name = site['name'] + "\n"
+            file_report.write(site_name)
 
-    for device in devices:
+        #Get tags on site
+        tags_url = "{}/sites/{}/wxtags".format(base_url, site["id"])
+        resultstags = requests.get(tags_url, headers=headers)
+        tags = json.loads(resultstags.text)
 
-        device_id = device['id']
-        device_name = device['name']
+        device_lookup = {}
+        device_lookup_mac = {}
 
-        device_lookup[device_id] = device_name
+        for device in devices:
 
-    #print (device_lookup)
+            device_id = device['id']
+            device_name = device['name']
+            device_mac = device['mac']
+
+            device_lookup[device_id] = device_name
+            device_lookup_mac[device_id] = device_mac
+
+        #print (device_lookup)
 
 
 
-    for tag in tags:
-        #Logs ID and name
-        logging.info(tag['name'])
-        logging.info('Tag-ID:')
-        logging.info(tag['id'])
-        numberaps = 0
-        for ap in tag['values']:
-            #Logs AP ID
-            logging.info('AP-ID:')
-            logging.info(ap)
-            #print(device_lookup[ap])
-            numberaps += 1
+        for tag in tags:
+            #Logs ID and name
+            logging.info(tag['name'])
+            logging.info('Tag-ID:')
+            logging.info(tag['id'])
+            numberaps = 0
+            for ap in tag['values']:
+                #Logs AP ID
+                logging.info('AP-ID:')
+                logging.info(ap)
+                #print(device_lookup[ap])
+                numberaps += 1
 
-            dict = {
-                "device_name": device_lookup[ap],
-                "device_tag": tag['name'],
-                "device_adress": site['name'],
-            }
-            dict_data.append(dict)
-
-        print(tag['name'], " ", numberaps)
+                dict = {
+                    "device_name": device_lookup[ap],
+                    "device_tag": tag['name'],
+                    "device_adress": site['name'],
+                    "device_mac": device_lookup_mac[ap],
+                }
+                dict_data.append(dict)
+            if numberaps != 0:
+                print(tag['name'], " ", numberaps)
+                forvaltning_ap = tag['name'] + " " + str(numberaps) + "\n"
+                file_report.write(forvaltning_ap)
 #print(dict_data)
+file_report.close()
 
-column_headers = ["device_name", "device_tag", "device_adress"]
+column_headers = ["device_name", "device_tag", "device_adress", "device_mac"]
 
 try:
-    with open(report_file, 'w', newline='') as csvfile:
+    with open(report_file_ap, 'w', newline='', encoding="utf8") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=column_headers)
         writer.writeheader()
 
@@ -95,6 +119,6 @@ try:
 
 except IOError as err:
     logging.error("CSV I/O error: {}".format(err))
-        #print(tag['name'], " ",numberaps)
+    #print(tag['name'], " ",numberaps)
     #for device in devices:
-        #print(device['name'], ">", device["id"], ">", )
+    #print(device['name'], ">", device["id"], ">", )
