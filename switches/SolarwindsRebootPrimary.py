cf1a6c206389ab40ba5f42b0d2f41a04235116b2
Samuel Sjöbergsson <33666789+sjobergsson@users.noreply.github.com>
2022-03-18 14:16:43 +0100

    Comments SolarwindsReboot and addVlan

--- switches/SolarwindsRebootPrimary.py
+++ switches/SolarwindsRebootPrimary.py
@@ -3,19 +3,24 @@ import orionsdk
 import yaml
 import json
 import pandas
+
 #Email imports
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+#Used for communicating with the switches
 from scrapli import Scrapli
 
+#open configuration file for use in the script
 with open('/opt/scripts/ap/config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+#Log to a specified file
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename='/opt/scripts/logs/rebootPrimary.log', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
 
+#set variables for easy access to settings
 switch_username = config['switch']['username']
 switch_password = config['switch']['password']
 solarwinds_username = config['solarwinds']['username']
@@ -34,8 +39,9 @@ message["Subject"] = "Netscript Juniper boot backup"
 message["From"] = sender_email
 message["To"] = receiver_email
 
-
+#Function that takes a switches IP-address and reboot it from the primary boot partition
 def rebootPrimary(switch_ip):
+    #Set information to the connection with scrapli
     device = {
         "host": switch_ip,
         "auth_username": switch_username,
@@ -44,10 +50,13 @@ def rebootPrimary(switch_ip):
         "platform": "juniper_junos"
     }
     logging.info("Logging in to switch {}".format(switch_ip))
+    #Connect to the switch
     try:
         conn = Scrapli(**device)
         conn.open()
+        #Save the used configuration to backup before rebooting
         response = conn.send_command("request system configuration rescue save")
+        #Schedule system reboot using the switch internal schedule
         response = conn.send_interactive(
             [
                 ("request system reboot slice alternate media internal at 22",
@@ -57,31 +66,36 @@ def rebootPrimary(switch_ip):
         )
         logging.info(response.elapsed_time)
         logging.info(response.result)
+        #Check whether the switch response is correct
         if 'Shutdown at' in response.result:
             return True
         else:
+            #Return false to know which switches worked and which dose´nt
             return False
         conn.close()
     except:
+        #If error log that it cant reboot switch
         logging.warning("Couldn't reboot switch {}".format(switch_ip))
-        print("Couldn't reboot switch {}".format(switch_ip))
         return False
 
 
-
+#Log in to Solarwinds
 session = requests.Session()
 session.timeout = 30  # Set your timeout in seconds
 logging.info("Connecting to Solarwinds")
 swis = orionsdk.SwisClient("SolarWinds-Orion", solarwinds_username,
                            solarwinds_password, verify=False, session=session)
 logging.info("Getting switches that need to reboot")
+#Solarwinds query to get switches that need to be rebooted
 nodes = swis.query("SELECT NodeID, Caption, IPAddress, Status, Nodes.CustomProperties.jun_bootpart FROM Orion.Nodes WHERE Nodes.CustomProperties.jun_bootpart LIKE 'backup%'")
-
+#get the list of switches from det result
 switches = nodes['results']
 logging.debug(switches)
 
+#create empty dict for the switches
 dict_switches = []
 
+#Try to reboot all the switches
 for switch in switches:
     if rebootPrimary(switch['IPAddress']):
         dict_switch = {
@@ -98,7 +112,7 @@ for switch in switches:
         }
         dict_switches.append(dict_switch)
 logging.info(json.dumps(dict_switches, indent=2, default=str))
-
+#Turn into a dataframe to include it in the mail
 df_switches = pandas.DataFrame(dict_switches)
 
 #Mail the result
@@ -122,6 +136,7 @@ part1 = MIMEText(text, "plain")
 part2 = MIMEText(html, "html")
 message.attach(part1)
 message.attach(part2)
+#Send mail only if there are any switches
 # send your email
 if dict_switches:
     with smtplib.SMTP(smtp_server, smtp_port) as server:
--- switches/addVlanToMistSwitches.py
+++ switches/addVlanToMistSwitches.py
@@ -1,22 +1,26 @@
 import requests
 import json
 import yaml
+#Used to get the switches from solarwinds
 import orionsdk
+#Used to work with xml from the switches
 import xmltodict
 
 from re import search
+#Used for communicating with the switches
 from scrapli import Scrapli
 
+#Log to a specified file
 import logging
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     filename='Mist-api.log', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
 
-
+#open configuration file for use in the script
 with open('../django-script/netscripts/mist_import_sites/config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-#base_url = config['mist']['base_url']
 
+#set variables for easy access to settings
 import_file = config['import']['import_file']
 
 import_file_sheet = config['import']['import_sheet']
@@ -38,8 +42,6 @@ org_id = config['mist']['org_id']  # Your Organization ID goes here
 
 base_url = config['mist']['base_url']
 
-printer_name = config['printing']['label_printer']
-label_file = config['printing']['file_name']
 
 authorization = "Token {}".format(mist_api_token)
 
@@ -47,7 +49,7 @@ headers = {
     'Content-Type': 'application/json',
     'Authorization': authorization
 }
-
+#Get shortname from sitename
 regex_shortname = ".*\((....)\)"
 
 ##Switch variables
@@ -57,7 +59,9 @@ solarwinds_username = config['solarwinds']['username']
 solarwinds_password = config['solarwinds']['password']
 solarwinds_certificate = config['solarwinds']['certificate_file']
 
+#Function that takes a switch object and add Vlan to that switch
 def addVlansToSwitch(switch):
+    #create device object for scrapli
     device = {
         "host": switch['IPAddress'],
         "auth_username": switch_username,
@@ -67,16 +71,23 @@ def addVlansToSwitch(switch):
     }
     logging.info("Logging in to switch {} with IP {}".format(
         switch['Caption'], switch['IPAddress']))
+    #Connecting to the switch
     conn = Scrapli(**device)
     conn.open()
+    #Check what kind of switch it is
     response = conn.send_command("show chassis hardware | display xml")
     response_dict = xmltodict.parse(response.result)
     response_json = json.dumps(response_dict)
     switch_hardware = json.loads(response_json)[
         'rpc-reply']['chassis-inventory']['chassis']
+    #Get the model from the hardware section in the switch
     switch_model = switch_hardware['description']
     print(switch_model)
+    #Based on switchmodel use different commands for adding a vlan
+    #### Here is where you change what VLAN you want to add
+    #### Make sure to change it on both EX2300 and EX2200
     if "EX2300" in switch_model:
+        #Check if there is a uplink interface-range and if so add the vlan to it
         response = conn.send_command(
             "show configuration interfaces interface-range uplink")
         if "unit 0" in response.result:
@@ -88,6 +99,8 @@ def addVlansToSwitch(switch):
             "set interfaces interface-range ap unit 0 family ethernet-switching vlan members chromebook")
         response = conn.send_config(
             "set vlans chromebook vlan-id 40")
+        #Use commit confirmed so that if i cut my leg off the switch will come back up in 5 minutes
+        # and add a comment so that its easy to see in the switch that it the config was scripted
         response = conn.send_config(
             'commit confirmed 5 comment "MIST preparation"')
         response = conn.send_config(
@@ -104,49 +117,65 @@ def addVlansToSwitch(switch):
             "set vlans chromebook interface ap")
         response = conn.send_config(
             "set vlans chromebook vlan-id 40")
+        #Use commit confirmed so that if i cut my leg off the switch will come back up in 5 minutes
+        # and add a comment so that its easy to see in the switch that it the config was scripted
         response = conn.send_config(
             'commit confirmed 5 comment "Chromebook-vlan preparation"')
         response = conn.send_config(
             'commit')
 
-
+    #Looging and output the response from the commit and the time
     logging.info(response.elapsed_time)
     logging.info(response.result)
     print(response.result)
+    #Disconnect from the switch
     conn.close()
 
+#Function that finds switches in solarwinds based on a shortname
 def findSwitches(shortname):
 
     print('\n\n==========\n\n')
     print("Adding Vlans byod and guest to switches on site")
+    #Connect to Solarwinds
     session = requests.Session()
     session.timeout = 30  # Set your timeout in seconds
     logging.info("Connecting to Solarwinds")
     swis = orionsdk.SwisClient("SolarWinds-Orion", solarwinds_username,
                                 solarwinds_password, verify=False, session=session)
     logging.info("Getting switches that belong to the site")
+    #Solarwinds Query to get access-switches that belongs to that shortname and are online according to solarwinds
     nodes = swis.query(
         "SELECT NodeID, Caption, IPAddress, Status FROM Orion.Nodes WHERE Caption LIKE 'swa-%-{}' AND Status LIKE 1".format(shortname))
+    #Reset switch number to zero
     switch_number = 0
+    #Convert result to a list of switches
     switches = nodes['results']
+    #Use the function to addVlanToSwitch
     for switch in switches:
         print(switch)
         addVlansToSwitch(switch)
+        #add number to switches to keep track of how many is updated
         switch_number += 1
 
     print("Updated settings on {} switches".format(switch_number))
+#Get the sites from Mist
 try:
+    #Mist API URL to get sites
     sites_url = "{}/orgs/{}/sites".format(base_url, org_id)
 
     resultssites = requests.get(sites_url, headers=headers)
+    #Convert the result into a list of sites
     sites = json.loads(resultssites.text)
 
+    #Go through sites and run the findSwitches functions with the shortname
     for site in sites:
         if search(regex_sitename, site['name']):
             print(site['name'])
+            #get the shortname of the site
             shortname = search(regex_shortname, site['name']).group(1)
             findSwitches(shortname)
             print('Vlan tillagda till site {}'.format(site['name']))
 
 except:
+    #If it dosen´t work to list the sites active in Mist
     print("Kan inte lista siter")
